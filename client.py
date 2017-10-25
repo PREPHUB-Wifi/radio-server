8ca2dab5e065d4152b42a44f06ea6f5c7b2da85d
Leah Goggin <lgoggin@mit.edu>
2017-10-25 18:12:18 -0400

    various test files

---
+++ client.py
@@ -0,0 +1,10 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = raw_input("Server hostname or ip? ")
+port = input("Server port? ")
+sock.connect((host,port))
+while True:
+    data = raw_input("message: ")
+    sock.send(data)
+    print "response: ", sock.recv(1024)
---
+++ radio.py
@@ -0,0 +1,4 @@
+import serial
+ser = serial.Serial('/dev/cu.usbmodem1421', 9600)
+while True:
+	print ser.readline()
