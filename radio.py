e9bc05d0a7e0825fa601d85be24fd9b9abdbcabb
Leah Goggin <lgoggin@mit.edu>
2017-10-25 20:40:51 -0400

    still figuring out how it makes sense to structure this

--- radio.py
+++ radio.py
@@ -1,4 +1,20 @@
 import serial
-ser = serial.Serial('/dev/cu.usbmodem1421', 9600)
-while True:
-	print ser.readline()
+
+class Radio(Object):
+	def __init__(port, baud):
+		self.port = port
+		self.baud = baud
+		ser = serial.Serial(port, baud)
+		time.sleep() # give time to initialize
+
+	def listen():
+		return ser.readline() #read(num) for bytes. timeout?
+
+	def send(message):
+		ser.write(message)
+
+
+if __name == "__main__":
+	port = '/dev/cu.usbmodem1421'
+	baud = 9600
+	radio = Radio(port, baud)
