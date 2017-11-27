d21693905a76c63251fdf843d7610714c4c937cc
Rachana Lingutla <rlingutla@umass.edu>
2017-11-26 19:41:25 -0500

    Put more logic in for node.js to python connection

--- testing-push.py
+++ testing-push.py
@@ -1,22 +1,32 @@
 from urllib.request import urlopen
-from urllib.parse import urlencode 
+from urllib.parse import urlencode  
+import urllib.request
 import http.client 
 import time
 from datetime import date
 
-url = '127.0.0.1:8081/notes' #node.js server address
-#test data 
-data = { 
+url = '127.0.0.1:8081/notes'
+data = {
   "newName":"Bridget",
   "needHelp":"food",
   "notes":"quesadilla",
   "time": 12345678923456789
 }
 
+curr_data = server_connection('GET') 
+if(data not in curr_data):
+  if the ids are out of sync:
+#   figure out which packets are missing
+#   send a request to the radio to get the missing packets 
+  server_connection('POST') 
 
-data_encoded = urlencode(data) #encode URL for sending
-h = http.client.HTTPConnection('127.0.0.1:8081') #connect to server
-headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"} #set headers
-h.request('POST', '/notes', data_encoded, headers) #post to server
-r = h.getresponse() #get the response
-print(r.read()) #print it out for a sanity check
+def fix_data(data):
+    return data 
+
+def server_connection(request_name):
+  data_encoded = urlencode(data)
+  h = http.client.HTTPConnection('127.0.0.1:8081')
+  headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+  h.request(request_name, '/notes', data_encoded, headers)
+  r = h.getresponse()
+  return r.read()
